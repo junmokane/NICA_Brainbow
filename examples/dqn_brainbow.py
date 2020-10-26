c097b9b8d5049e42d9a6947b1b8bb4d531d8b391
junmokane <junmokane@kaist.ac.kr>
2020-10-26 19:33:55 +0900

    dqn bb

--- examples/dqn_brainbow.py
+++ examples/dqn_brainbow.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
         layer_size=256,
         replay_buffer_size=int(1E6),
         algorithm_kwargs=dict(
-            num_epochs=10,
+            num_epochs=1000,
             num_eval_steps_per_epoch=5000,
             num_trains_per_train_loop=1000,
             num_expl_steps_per_train_loop=1000,
@@ -137,7 +137,7 @@ if __name__ == "__main__":
                  variant_log_file="variant.json",
                  tabular_log_file="progress.csv",
                  snapshot_mode="gap_and_last",
-                 snapshot_gap=2,
+                 snapshot_gap=50,
                  log_tabular_only=False,
                  log_dir=None,
                  git_infos=None,
